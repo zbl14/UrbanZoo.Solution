4fa4027ace6b1ff41343def7eaff342eece8180f
MoseyByrne <mosey.b.byrne@gmail.com>
2022-08-22 15:16:09 -0700

    run migration and build
    Co-authored-by: Winnie Wang
    <weijwang18@gmail.com>
    Co-authored-by: Tyler Emmerson <tyleremmerson0216@gmail.com>

--- UrbanZoo_Client/Models/ErrorViewModel.cs
+++ UrbanZoo_Client/Models/ErrorViewModel.cs
@@ -1,8 +1,11 @@
-namespace UrbanZoo.Models;
+using System;
+
+namespace UrbanZoo.Models {
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
 }

ac054a8490cc41048b1ecc89761fcf1143bcb88d
jacobpalaoro <jpalaoro197@gmail.com>
2022-08-23 13:27:52 -0700

    base line project with data bug.

--- UrbanZoo_Client/Startup.cs
+++ UrbanZoo_Client/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace CretaceousClient
+namespace UrbanZoo
 {
     public class Startup
     {

b87103063c41823d5481137dbaf9cea0f0016c1d
zbl14 <ifthereisoneday@gmail.com>
2022-08-25 15:16:30 -0700

    BUGFIX: add resolve conflicting action for swagger service

--- UrbanZoo_API/Startup.cs
+++ UrbanZoo_API/Startup.cs
@@ -9,6 +9,9 @@ using UrbanZoo.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Linq;
 
 namespace UrbanZoo
 {
@@ -39,7 +42,43 @@ namespace UrbanZoo
             });
             
             services.AddControllers();
-            services.AddSwaggerGen();
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Title = "UrbanZoo", 
+                    Version = "v1",
+                    Description = "An ASP.NET Core Web API for UrbanZoo",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Zhibin Liang",
+                        Email = "ifthereisoneday@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/zhibin-liang"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "No license requied"
+                    }
+                });
+                c.SwaggerDoc("v2", new OpenApiInfo
+                {
+                    Title = "UrbanZoo", 
+                    Version = "v2",
+                    Description = "An ASP.NET Core Web API for UrbanZoo",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Zhibin Liang",
+                        Email = "ifthereisoneday@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/zhibin-liang"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "No license requied"
+                    }
+                });
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());            
+            });
 
             services.AddApiVersioning(opt =>
             {
@@ -54,17 +93,21 @@ namespace UrbanZoo
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Free Feature Finder V1");
-            });
-            
+        {          
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger(c => 
+            {
+                c.SerializeAsV2 = true;
+            });
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "UrbanZoo v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             // app.UseHttpsRedirection();
 
